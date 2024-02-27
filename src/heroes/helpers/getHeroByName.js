6fc1d872f0f439913edf365fe16b2b209151c1d6
jeune07 <jeunewinsley9@gmail.com>
2024-02-26 23:54:56 -0300

    empty commit

--- src/heroes/helpers/getHeroByName.js
+++ src/heroes/helpers/getHeroByName.js
@@ -3,5 +3,5 @@ import { heroes } from "../data/heroes";
 export const getHeroByName = (name = "") => {
   name = name ? name.toLowerCase().trim() : "";
   if (name.length <= 0) return [];
-  return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
+  return heroes.filter(hero => hero.superhero.toLowerCase().includes(name))
 };
